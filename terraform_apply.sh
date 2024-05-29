34204a5411a952ab5f96561ad4e1ecc40b064504
Agus <agutierrez@itba.edu.ar>
2024-05-28 23:31:40 -0300

    Add lambdas parallelism

--- terraform_apply.sh
+++ terraform_apply.sh
@@ -3,10 +3,15 @@ cd terraform
 
 terraform init
 
-# Apply the entire Terraform configuration to create the build files
+# Lambdas are created in parallel due to their long creation time
+terraform apply -auto-approve -target=module.lambdas
+
+sleep 10
+
+# Serial processing needed to prevent concurrency errors in gateway integration creation
 terraform apply -auto-approve -parallelism=1
 
-sleep 5
+sleep 10
 
-# Apply the Terraform configuration again, but only for the aws_s3_object.object resource
+# The newly built frontend is uploaded to the S3 bucket in a separate step, to workaround the fileset function only scanning during plan, not on execution
 terraform apply -auto-approve -target=aws_s3_object.object
